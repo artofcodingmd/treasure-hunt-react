2fb3d4e69316dc1d57a46572f099607327421f2c
artofcodingmd <243574134+artofcodingmd@users.noreply.github.com>
2026-01-27 13:46:05 -0800

    chore(legacy-shell): Fix bases for dev vs production and fix legacy image path in CSS

--- legacy-shell/vite.config.js
+++ legacy-shell/vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: 'treasure-hunt-react/legacy-shell/' // Set base path for correct asset loading
-})
+  base: 
+    mode === 'production'
+    ? 'treasure-hunt-react/legacy-shell/legacy/' 
+    : '/'
+})); // Set base path for production and development modes
